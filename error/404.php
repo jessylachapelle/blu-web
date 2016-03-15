5e15d2a05def37797e0f50d86813e2abff63645a
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2016-03-14 22:10:31 -0400

    Json et error files

---
+++ error/404.php
@@ -0,0 +1,4 @@
+<h1>Erreur 404 - Page non trouvée</h1>
+<p>
+  Il semblerait que cette page n'existe pas. Si vous pensez qu'il s'agit d'une erreur de notre part, merci de nous le signaler en nous envoyons un courriel en décrivant le problème. Merci d'avance.
+</p>
