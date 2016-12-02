727a8e977f74f538d97b24747476bc27dc9c0213
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2016-12-02 18:24:27 -0500

    Fix error reporting

--- res/signalement.php
+++ res/signalement.php
@@ -11,16 +11,16 @@ if (isset($_POST['signal']) && isset($_POST['article']) && isset($_POST['memberN
   mysqli_query($connection, $query) or die("Query failed: '$query' " . mysqli_error());
   mysqli_close($connection);
 
-  return redirect($articleId, 200);
+  return redirect($item, 200);
 }
 
 return redirect($_POST['article'], 403);
 
-function redirect($articleId, $code) {
+function redirect($item, $code) {
   if ($code == 200) {
-    header("Location: ../article.php?article=$articleId&signaled=true");
+    header("Location: ../article.php?article=$item&signaled=true");
   } else {
-    header("Location: ../article.php?article=$articleId&error=$code");
+    header("Location: ../article.php?article=$item&error=$code");
   }
 }
 ?>
