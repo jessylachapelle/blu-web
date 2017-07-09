8c3fc39a070b40383792824e82461a870529c4ba
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2017-07-09 19:27:11 -0400

    Minor fixes

--- inc/nav.php
+++ inc/nav.php
@@ -1,7 +1,7 @@
 <nav id="menu">
 	<a href="index.php">MON COMPTE</a>
 	<a href="apropos.php">À PROPOS</a>
-	<a href="message.php">NOUVELLES</a>
+	<!--<a href="message.php">NOUVELLES</a>-->
 	<a href="contact.php">CONTACTEZ-NOUS</a>
 	<a href="wtfaq.php">WTFAQ</a>
 </nav>
