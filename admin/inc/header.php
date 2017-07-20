5e460239a9ea9be9e5d93d5453bc24ff3b5688f5
Jessy Lachapelle <jessy.lachapelle@gmail.com>
2017-07-20 12:45:08 -0400

    Remove admin pages

--- admin/inc/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-	<img src="../img/logo.png" alt="logo" />
-	<form class="nostyle">
-		<input id="search" type="search" placeholder="Chercher un livre" />
-	</form>
-	<?php include "inc/nav.php"; ?>
-</header>
--- admin/inc/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-	<a href="admin.php">ADMIN</a>
-	<a href="statistiques.php">STATISTIQUES</a>
-	<a href="nouvelles.php">NOUVELLES</a>
-	<a href="signalement.php">RAPPORT D'ERREURS</a>
-	<?php // <a href="employés.php">EMPLOYÉS</a> ?>
-</nav>
